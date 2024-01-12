6db2b38118d3445a595fca7514b5cc8250c2a8c0
pp0503 <143099940+pp0503@users.noreply.github.com>
2024-01-12 09:45:28 +0530

    by git1

--- Git_program/src/com/cjc/Demo/Demo.java
+++ Git_program/src/com/cjc/Demo/Demo.java
@@ -6,7 +6,10 @@ public void m1()
 {
 	System.out.println("m1 method");
 }
-
+public void m2()
+{
+	System.out.println("m2 method");
+}
 public static void main(String[] args) 
 {
 	System.out.println("Hello Git");		
